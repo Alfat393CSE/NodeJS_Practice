ae2188a4fb74f82f3715331af5d12d51b3a2c5bf
Alfat393CSE <alfattasnimhasan@gmail.com>
2026-08-31 23:34:22 +0600

    response sending with different pages understand

--- 03_Request_Response/script.js
+++ 03_Request_Response/script.js
@@ -2,9 +2,12 @@ const http = require("http");
 const PORT = 3000;
 
 const server = http.createServer((req, res) => {
-  res.setHeader("Content-type", "text/html");
-  res.write(
-    `<html>
+  console.log(req.url, req.method, req.headers);
+
+  if (req.url === "/") {
+    res.setHeader("Content-type", "text/html");
+    res.write(
+      `<html>
       <head>
         <title>Node JS</title>
       </head>
@@ -12,8 +15,35 @@ const server = http.createServer((req, res) => {
         <h1> this is Alfat</h1>
       </body>
     </html>`,
+    );
+    return res.end();
+  } else if (req.url === "/products") {
+    res.setHeader("Content-type", "text/html");
+    res.write(
+      `<html>
+      <head>
+        <title>Node JS</title>
+      </head>
+      <body>
+        <h1> Products</h1>
+      </body>
+    </html>`,
+    );
+    return res.end();
+  }
+  res.setHeader("Content-type", "text/html");
+  res.write(
+    `<html>
+      <head>
+        <title>Node JS</title>
+      </head>
+      <body>
+        <h1> this is ordinary one</h1>
+      </body>
+    </html>`,
   );
   res.end();
+
   // process.exit(); // stop the server after logged the request
 });
 

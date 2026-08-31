bc786352173ade8e001794ef435c2d4dd12765e1
Alfat393CSE <alfattasnimhasan@gmail.com>
2026-09-01 00:16:25 +0600

    taking user input and redirecting the request

--- 03_Request_Response/script.js
+++ 03_Request_Response/script.js
@@ -1,4 +1,5 @@
 const http = require("http");
+const fs = require("fs");
 const PORT = 3000;
 
 const server = http.createServer((req, res) => {
@@ -12,24 +13,22 @@ const server = http.createServer((req, res) => {
         <title>Node JS</title>
       </head>
       <body>
-        <h1> this is Alfat</h1>
+        <form action = "/submit-task" method = "POST">
+        <input type="text" id="name" name="name" placeholder="enter your name">
+        <label for="male">Male</label>
+        <input type="radio" id="male" name="gender" value="male" />
+        <label for="female">Female</label>
+        <input type="radio" id="female" name="gender" value="female" />
+        <button type="submit">Submit</button>
+        </form> 
       </body>
     </html>`,
     );
     return res.end();
-  } else if (req.url === "/products") {
-    res.setHeader("Content-type", "text/html");
-    res.write(
-      `<html>
-      <head>
-        <title>Node JS</title>
-      </head>
-      <body>
-        <h1> Products</h1>
-      </body>
-    </html>`,
-    );
-    return res.end();
+  } else if (req.url.toLowerCase() === "/submit-task" && req.method === "POST") {
+    fs.writeFileSync("./03_Request_Response/user.txt", "Alfat Tasnim Hasan");
+    res.statusCode = 302;
+    res.setHeader("Location", "/");
   }
   res.setHeader("Content-type", "text/html");
   res.write(
@@ -38,11 +37,11 @@ const server = http.createServer((req, res) => {
         <title>Node JS</title>
       </head>
       <body>
-        <h1> this is ordinary one</h1>
+        <h1> this is Alfat</h1>
       </body>
     </html>`,
   );
-  res.end();
+  return res.end();
 
   // process.exit(); // stop the server after logged the request
 });

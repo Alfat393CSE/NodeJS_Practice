0110aff7a8b9d0899449fbbfacd8a40bc82a1bca
Alfat393CSE <alfattasnimhasan@gmail.com>
2026-08-31 22:55:45 +0600

    understand the request using url, method and headers

--- 03_Request_Response/script.js
+++ 03_Request_Response/script.js
@@ -2,8 +2,8 @@ const http = require("http");
 const PORT = 3000;
 
 const server = http.createServer((req, res) => {
-  console.log(req);
-  process.exit(); // stop the server after logged the request
+  console.log(req.url, req.method, req.headers);
+  // process.exit(); // stop the server after logged the request
 });
 
 server.listen(PORT, () => {

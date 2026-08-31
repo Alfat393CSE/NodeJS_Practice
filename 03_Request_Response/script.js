c57b681b9ec187e473bf31eea5c271f86d079c49
Alfat393CSE <alfattasnimhasan@gmail.com>
2026-08-31 23:20:04 +0600

    response sending understand

--- 03_Request_Response/script.js
+++ 03_Request_Response/script.js
@@ -2,7 +2,18 @@ const http = require("http");
 const PORT = 3000;
 
 const server = http.createServer((req, res) => {
-  console.log(req.url, req.method, req.headers);
+  res.setHeader("Content-type", "text/html");
+  res.write(
+    `<html>
+      <head>
+        <title>Node JS</title>
+      </head>
+      <body>
+        <h1> this is Alfat</h1>
+      </body>
+    </html>`,
+  );
+  res.end();
   // process.exit(); // stop the server after logged the request
 });
 

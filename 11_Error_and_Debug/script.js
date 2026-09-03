de1acd954e8ead4a4a724ca0398d6251d6814958
Alfat393CSE <alfattasnimhasan@gmail.com>
2026-09-03 15:40:47 +0600

    understand the error and how to debug the errors

---
+++ 11_Error_and_Debug/script.js
@@ -0,0 +1,15 @@
+function calculateArea(width, height) {
+  return width + height;
+}
+let width = 10,
+height = 5;
+let area = width * height;
+if (area > 100) {
+  console.log("the area is large");
+} else {
+  console.log("the area is small");
+}
+
+if (width + height > 100) {
+  console.log("area is greater than or equal to 100");
+}

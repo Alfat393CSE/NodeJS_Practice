2f8acc407a08e35b750a742256aaf55edcbb730c
Alfat393CSE <alfattasnimhasan@gmail.com>
2026-09-04 16:49:38 +0600

    load the file paths perfectly

--- 13_Server_Creation/server.js
+++ 13_Server_Creation/server.js
@@ -4,6 +4,22 @@ const path = require("path");
 const PORT = 3000;
 
 const server = http.createServer((req, res) => {
+  const filePath = path.join(
+    __dirname,
+    req.url === "/" ? "index.html" : req.url,
+  );
+
+  const extName = path.extname(filePath).toLowerCase();
+
+  let mimeType = {
+    ".html": "text/html",
+    ".css": "text/css",
+    ".js": "text/javascript",
+    ".png": "text/png",
+  };
+
+  const contentType = mimeType[extName] || "application/octet-stream";
+
   
 });
 

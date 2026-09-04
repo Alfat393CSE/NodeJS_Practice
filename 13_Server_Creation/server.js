f7f4e419081988e76765e8306939db6eee4ce895
Alfat393CSE <alfattasnimhasan@gmail.com>
2026-09-04 16:48:03 +0600

    running the server successfully

---
+++ 13_Server_Creation/server.js
@@ -0,0 +1,12 @@
+const http = require("http");
+const fs = require("fs");
+const path = require("path");
+const PORT = 3000;
+
+const server = http.createServer((req, res) => {
+  
+});
+
+server.listen(PORT, () => {
+  console.log(`server is running at http://localhost:${PORT}`);
+});

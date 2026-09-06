587f38808e84e80fb8f0f4605c2b3a9d7a0c6f86
Alfat393CSE <alfattasnimhasan@gmail.com>
2026-09-06 19:24:18 +0600

    test three seperate middleware that successfully running

--- 16_Middleware_Practice_Set/app.js
+++ 16_Middleware_Practice_Set/app.js
@@ -3,6 +3,21 @@ const PORT = 3000;
 
 const app = express();
 
+app.use((req, res, next) => {
+  console.log("first middleware", req.url, req.method);
+  next();
+});
+
+app.use((req, res, next) => {
+  console.log("second middleware", req.url, req.method);
+  next();
+});
+
+app.use((req, res, next) => {
+  console.log("third middleware", req.url, req.method);
+  res.send(`<p>Welcome to testing middleware</p>`);
+});
+
 app.listen(PORT, () => {
   console.log(`server is running at http://localhost:${PORT}`);
 });

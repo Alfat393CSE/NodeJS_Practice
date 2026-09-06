b997c058d9c1c906a13d74914abc216b1b5d7028
Alfat393CSE <alfattasnimhasan@gmail.com>
2026-09-06 19:35:19 +0600

    implemented contact-us by get method and get response by post

--- 16_Middleware_Practice_Set/app.js
+++ 16_Middleware_Practice_Set/app.js
@@ -13,9 +13,29 @@ app.use((req, res, next) => {
   next();
 });
 
-app.use((req, res, next) => {
-  console.log("third middleware", req.url, req.method);
-  res.send(`<p>Welcome to testing middleware</p>`);
+// app.use((req, res, next) => {
+//   console.log("third middleware", req.url, req.method);
+//   res.send(`<p>Welcome to testing middleware</p>`);
+// });
+
+app.get("/", (req, res, next) => {
+  console.log("fourth middleware", req.url, req.method);
+  res.send(`<h1>Welcome to HomePage</h1>`);
+});
+
+app.get("/contact-us", (req, res, next) => {
+  console.log("fifth middleware", req.url, req.method);
+  res.send(`<h1>Please enter your detailes</h1>
+    <form action="/contact-us" method="POST">
+    <input type="text" name="name" placeholder="enter your name">
+    <input type="email" name="email" placeholder="enter your email">
+    <input type="Submit">
+  </form>;`);
+});
+
+app.post("/contact-us", (req, res, next) => {
+  console.log("sixth middleware", req.url, req.method);
+  res.send(`<p>We will contact you soon</p>`);
 });
 
 app.listen(PORT, () => {

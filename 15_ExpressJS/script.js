a65e44b5e725b4903627b4bb59b2bd8039010419
Alfat393CSE <alfattasnimhasan@gmail.com>
2026-09-06 17:46:53 +0600

    understand the app.get, app.post, app.use and middleware concepts

--- 15_ExpressJS/script.js
+++ 15_ExpressJS/script.js
@@ -3,6 +3,21 @@ const PORT = 3000;
 
 const app = express();
 
+app.get("/", (req, res, next) => {
+  console.log("first middleware", req.url, req.method);
+  // res.send(`<p>Welcome to first middleware</p>`);
+  next();
+});
+
+app.post("/pages", (req, res, next) => {
+  console.log("second middleware", req.url, req.method);
+  res.send(`<p>Welcome to second middleware</p>`);
+});
+
+app.use("/", (req, res, next) => {
+  console.log("third middleware", req.url, req.method);
+  res.send(`<p>Welcome to third middleware</p>`);
+});
 app.listen(PORT, () => {
   console.log(`server is running at http://localhost:${PORT}`);
 });

28a6d0953c539d0cb70df82ee32b67a1cc6b22f2
Alfat393CSE <alfattasnimhasan@gmail.com>
2026-09-07 16:18:46 +0600

    added the routes and utilities such as path"

--- 17_AirBnB/app.js
+++ 17_AirBnB/app.js
@@ -1,9 +1,21 @@
 const express = require("express");
+const path = require("path");
 
-const PORT = 3000;
+const hostRoute = require("./routes/hostRoutes");
+const userRoute = require("./routes/userRoutes");
+const rootDir = require("./utils/pathUtils");
 
 const app = express();
 
+app.use(express.urlencoded());
+app.use("/host", hostRoute);
+app.use(userRoute);
+
+app.use((req, res, next) => {
+  res.status(404).sendFile(path.join(rootDir, "views", "404.html"));
+});
+
+const PORT = 3000;
 app.listen(PORT, () => {
   console.log(`server running at http://localhost:${PORT}`);
 });
